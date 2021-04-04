ff7d1211a200b9eea2daf6252476c2a485253a57
EmilioAngelM <77894543+EmilioAngelM@users.noreply.github.com>
2021-04-04 18:15:05 -0400

    WIP - Started to reduce complexity

--- app/game.py
+++ app/game.py
@@ -24,23 +24,45 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+#if u == "rock" and c == "rock":
+#    print("It's a tie!")
+#elif u == "rock" and c == "paper":
+#    print("The computer wins")
+#elif u == "rock" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "paper" and c == "rock":
+#    print("The computer wins")
+#elif u == "paper" and c == "paper":
+#    print("It's a tie!")
+#elif u == "paper" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "scissors" and c == "rock":
+#    print("The computer wins")
+#elif u == "scissors" and c == "paper":
+#    print("The user wins")
+#elif u == "scissors" and c == "scissors":
+#    print("It's a tie!")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
-
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+if u == "rock":
+    if c == "rock":
+        print("It is a tie!")
+    elif c == "paper":
+        print("Oh, better luck next time. The computer won")
+    elif c == "scissors":
+        print("Great Job! You WON")
+elif u == "paper":
+    if c == "rock":
+        print("Great Job! You WON")
+    elif c == "paper":
+        print("It is a tie!")
+    elif c == "scissors":
+        print("Oh, better luck next time. The computer won")
+elif u == "scissors":
+    if c == "rock":
+        print("Oh, better luck next time. The computer won")
+    elif c == "paper":
+        print("Great Job! You WON")
+    elif c == "scissors":
+        print("It is a tie!")
